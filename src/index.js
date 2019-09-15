d2c28deb0c07de889bacdfb2f9ca83ee20d20ab6
Mike Harrison <harrisonm7@rogers.com>
2019-09-15 09:35:05 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// JS Goes here - ES6 supported
+
+import "./css/main.css";
+
+// Say hello
+console.log("🦊 Hello! Edit me in src/index.js");
+
+const subHeading = document.createElement('h2')
+subHeading.textContent = "My new Subheading"
+document.querySelector('body').appendChild(subHeading)
+
